7ba96db017030bba2f20ed194b80d38721ffef0a
guangLess <bha.guang@gmail.com>
2015-09-04 13:22:36 -0400

    BOOL/if/else

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -24,6 +24,21 @@
      
      */
     
+    NSString * talkToGrandma = @"HI, GRANDMA!";
+    NSString * shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"Hi, Grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    if (!shouting) {
+        NSLog(@"WHAT IS THAT? SPEAK UP, DEAR");
+    } else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    
     return YES; // Don't alter this statement.
 }
 
