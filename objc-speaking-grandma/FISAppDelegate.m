fc0e1237183217adfca480b8364ac40a616332e7
jen2 <sipila.jen@gmail.com>
2015-11-02 15:12:22 -0500

    Completed talking Grandma lesson!
    Learned about boolean comparison and the function comparison using
    isEqualToString.  Pretty awesome!

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,11 +7,62 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    
+    NSString *talkToGrandma = @"Hi, Grandma! How are you?";
+    
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"HI, GRANDMA! HOW ARE YOU?";
+    
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    if (shouting)
+    {
+        
+        NSLog(@"No, I don't like sweet tea.");
+        
+    }else{
+        
+
+        NSLog(@"WHAT'S THAT, DEAR? MY HEARING AID ISN'T WORKING TODAY.");
+        
+        
+    }
+    
+    
+    //Direct evaluation.
+    
+    if ([talkToGrandma isEqualToString:shoutAtGrandma]){
+        
+        NSLog(@"No, I don't like sweet tea");
+    
+    }else{
+        
+        NSLog(@"WHAT'S THAT, DEAR? MY HEARING AID ISN'T WORKING TODAY.");
+        
+    }
+    
+    
+    //Inverted Check
+    
+    if (!shouting){
+        NSLog(@"WHAT'S THAT, DEAR? MY HEARING AID ISN'T WORKING TODAY.");
+    
+    }else{
+        NSLog(@"No, I don't like sweet tea.");
+        
+    }
+    
+    
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT, DEAR? MY HEARING AID ISN'T WORKING TODAY.");
+        
+    }else{
+        
+        NSLog(@"No, I don't like sweet tea");
+    }
     
     return YES; // Don't alter this statement.
 }
