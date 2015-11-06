e202e154b5df87c9bdeffb68f655ddd121a946a6
ariingber <ariingber@gmail.com>
2015-11-06 12:32:52 -0500

    Completed Speaking w/ Grandma Code-Along

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,11 +7,45 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    // I. Talk to Grandma
+    NSString *talkToGrandma = @"HI BUBIE, I LOVE YOU";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"Hi Bubie, I love you";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    if (shouting) {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    else {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    // II. Direct Evaluation
+    if ([talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"NO, NOT SINCE 1938!");
+    } else {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    // III. Inverted Check
+    if (!shouting) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    } else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    } else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    // IV. Advanced
+    
+    
+    
     
     return YES; // Don't alter this statement.
 }
