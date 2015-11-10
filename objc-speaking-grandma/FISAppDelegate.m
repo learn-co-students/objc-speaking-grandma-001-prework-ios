9d1986ca78b94ddee08d9775e69c4c2819d310d2
mariyaeggs <mariyawilson@att.net>
2015-11-10 08:53:30 -0500

    Add conditionals to obj-speaking-grandma

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -13,6 +13,47 @@
      
      */
     
+    // Part I (& Advanced)
+    
+    NSString*talkToGrandma=@"HI GRAMS!"; // allter from lowercase "Hi grams!" and "HI GRAMS!"
+    NSString*shoutAtGrandma=[talkToGrandma uppercaseString];
+    BOOL shouting=[talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    // advanced
+    
+    talkToGrandma=@"Hi grandma!"; // lowercase
+    shoutAtGrandma=[talkToGrandma uppercaseString]; // redefined
+    
+    if (shouting){
+        NSLog(@"NOT SINCE 1938!");
+    } else {
+        NSLog(@"WHAT'S THAT? SPEAK UP DEARIE DEAREST!");
+    }
+    
+    // Part II
+    
+    if([talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"NOT SINCE 1938!");
+        } else {
+            NSLog(@"WHAT'S THAT? SPEAK UP DEARIE DEAREST!");
+        }
+
+    // Part III
+    
+    if (!shouting){
+        NSLog(@"WHAT'S THAT? SPEAK UP DEARIE DEAREST!");
+    } else {
+        NSLog(@"NOT SINCE 1938!");
+    }
+    if(![talkToGrandma isEqualToString:shoutAtGrandma]) { // notice position of negation
+       NSLog(@"WHAT'S THAT? SPEAK UP DEARIE DEAREST!");
+    } else {
+        NSLog(@"NOT SINCE 1938!");
+    }
+    
+    // Advanced part see above in variable decleclarations
+    
+
     return YES; // Don't alter this statement.
 }
 
