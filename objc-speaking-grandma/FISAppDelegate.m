b132be65fced2259d55d9c5ef3c703d5df1d384f
aleclindz <alexander.v.lindsay@gmail.com>
2016-02-28 18:18:05 -0500

    Done.

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,11 +7,45 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    
+    
+    
+    NSString *talkToGrandma = @"WHEN WERE YOU BORN?";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"When were you born?";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    if (shouting) {
+        NSLog(@"%@", @"NO, NOT SINCE 1953!");
+    } else {
+        NSLog(@"WHAT'S THAT, DEAR?");
+    }
+    
+    if ([talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"I'M TALKING TO YOU!");
+    } else {
+        NSLog(@"COME AGAIN?");
+    }
+    
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"I'M TALKING TO YOU!");
+    } else {
+        NSLog(@"COME AGAIN?");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     return YES; // Don't alter this statement.
 }
