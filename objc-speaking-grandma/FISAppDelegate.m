ba5062e0d7893003ab8fa0cc6b66568405fc92f5
kenpcjr <kennethcooke@me.com>
2016-03-20 18:23:21 -0400

    Finish talking to Grandma.

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -13,6 +13,38 @@
      
      */
     
+    NSString *talkToGrandma = @"WE MISS YOU VERY MUCH GRANDMA!";
+    NSLog(@"%@", talkToGrandma);
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    NSLog(@"%@", shoutAtGrandma);
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma =@"We miss you very much Grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    NSLog(@"%d", shouting);
+    if (shouting) {
+        NSLog(@"NO, NOT SINCE 1938.");
+    }else{
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR.");
+    }
+    
+    if ([talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"NO, NOT SINCE 1938.");
+    }else{
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR.");
+    }
+    if (!shouting) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR");
+    }else{
+        NSLog(@"NO, NOT SINCE 1938.");
+    }
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR.");
+    }else{
+        NSLog(@"NO, NOT SINCE 1938.");
+    }
+    
     return YES; // Don't alter this statement.
 }
 
