dcccdfbe9f81f5eecfc79b8325b106c138f05986
priscim <priscim@aol.com>
2016-04-03 22:59:42 -0400

    Done.

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -11,6 +11,31 @@
      
      * Write your code here!
      
+    NSString * talkToGramma = @ "Talk to Gramma";
+//     NSString * shoutAtGramma = [talkToGramma uppercaseString];
+//     BOOL shouting = [talkToGramma isEqualToString: shoutAtGramma];
+     
+//  if (shouting) {
+//     NSLog(@ "No, not since 1938!"); }
+     
+//  if (shouting) {
+//     NSLog (@"No"); }
+//  else {
+//     NSLog (@"otherwise"); }
+  
+//     if([talkToGramma IsEqualToString: shoutAtGramma])
+//     {  NSLog(@"No"); }
+//     else
+//     {  NSLog(@"otherwise"); }
+     
+  
+//     if (! shouting) {
+//     NSLog(@"What dear"); }
+//     else
+//     { NSLog(@"No, 1938"); }
+     
+     
+     
      */
     
     return YES; // Don't alter this statement.
