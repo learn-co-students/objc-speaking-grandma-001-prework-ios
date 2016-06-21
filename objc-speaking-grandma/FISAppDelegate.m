4486140e9ed9a4849d786b87f6574faf2779ff08
Louis <Lharris134@gmail.com>
2016-06-21 01:39:50 -0400

    Boolean's

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -6,12 +6,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSString *talkToGrandma = @"Yo, Grandma!";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    if(!shouting)
+    {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    else
+    {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
     
     return YES; // Don't alter this statement.
 }
