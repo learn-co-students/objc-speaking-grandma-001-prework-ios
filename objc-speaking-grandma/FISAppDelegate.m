a0a0c442671a97a32fffd43e9852acbb2c95deb7
Michael <MH51893@aol.com>
2016-08-16 18:23:47 -0400

    Code Along 3 + Advanced

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -9,9 +9,16 @@
     
 //  Code Along 1 - Talk to Grandma
     
-    NSString *talkToGrandma = @"Hi, Grandma!";
+    NSString *talkToGrandma = @"HI, GRANDMA!";
     NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
     BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+//    Advanced - Code Along 4
+    
+    talkToGrandma = @"Hi, Grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+//    cont... Code Along 1
     
     if (shouting) {
         NSLog(@"NO, NOT SINCE 1938!");
@@ -31,8 +38,28 @@
     }
     
     
+//    Code Along 3 - Inverted Check
+    
+    if (!shouting) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR");
+    }else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR");
+    }else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    
+//    Advanced
+    
+    
+    
     
     return YES; // Don't alter this statement.
 
 }
+
 @end
