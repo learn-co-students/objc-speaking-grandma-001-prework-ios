8dfa7d65659e865ffab8d1681bb358bb393cf266
Michael <MH51893@aol.com>
2016-08-16 17:19:56 -0400

    Code Along 1 - Talk to Grandma

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,13 +7,23 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+//  Code Along 1 - Talk to Grandma
     
+    NSString *talkToGrandma = @"HI, GRANDMA!";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    if (shouting) {
+        NSLog(@"NO, NOT SINCE 1938!");
+        
+    } else {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    
+    }
     return YES; // Don't alter this statement.
 }
+
+
+//  Code Along 2 - Direct Evaluation
 
 @end
